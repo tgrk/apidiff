db53f4c242b6e6852bb80a1c3f2ca91ade2f285d
Martin Wiso <martin@wiso.cz>
2018-12-05 22:31:13 +0100

    Add manifest for describing API request

---
+++ manifest.go
@@ -0,0 +1,35 @@
+package apidiff
+
+import (
+	"bytes"
+	"io"
+
+	"gopkg.in/yaml.v2"
+)
+
+// Manifest holds all information needed for running
+// requests against API
+type Manifest struct {
+	Version  int           `yaml:"version"`
+	Requests []RequestInfo `yaml:"requests"`
+}
+
+// NewManifest creates an empty manifest
+func NewManifest() *Manifest {
+	return &Manifest{}
+}
+
+// Parse YAML document
+func (m *Manifest) Parse(r io.Reader) error {
+	buf := new(bytes.Buffer)
+	_, err := buf.ReadFrom(r)
+	if err != nil {
+		return err
+	}
+
+	err = yaml.Unmarshal(buf.Bytes(), m)
+	if err != nil {
+		return err
+	}
+	return nil
+}
